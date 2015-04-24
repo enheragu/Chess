ca4f2886d917195f841f5c610ce7c077ac6ce57d
Enrique Heredia Aguado <enrique.he.ag@gmail.com>
2015-04-24 20:21:58 +0200

    Update Tablero.h

--- Ajedrez/src/Logico/Tablero.h
+++ Ajedrez/src/Logico/Tablero.h
@@ -7,13 +7,13 @@ class Tablero
 		0			Casilla vacia			0
 		1			Peon sin mover			-1
 		2			Peon movido 2			-2
-		3			Caballo					-3
-		4			Alfil					-4
+		3			Caballo				-3
+		4			Alfil				-4
 		5			Torre no movida			-5
 		6			Torre movida			-6
-		7			Dama					-7
+		7			Dama				-7
 		8			Rey no movida			-8
-		9			Rey movido				-9
+		9			Rey movido			-9
 		255			Casilla prohibida		255
 
 	*/
