04554f8baf6cdbda4cba065ca18d7432b85d9849
Enrique Heredia Aguado <enrique.he.ag@gmail.com>
2015-04-25 12:25:57 +0200

    Update Tablero.h

--- Ajedrez/src/Logico/Tablero.h
+++ Ajedrez/src/Logico/Tablero.h
@@ -7,18 +7,17 @@ class Tablero
 		0			Casilla vacia			0
 		1			Peon sin mover			-1
 		2			Peon movido 2			-2
-		3			Caballo				-3
-		4			Alfil				-4
+		3			Caballo					-3
+		4			Alfil					-4
 		5			Torre no movida			-5
 		6			Torre movida			-6
-		7			Dama				-7
+		7			Dama					-7
 		8			Rey no movida			-8
-		9			Rey movido			-9
-		255			Casilla prohibida		255
-
+		9			Rey movido				-9
+	   255			Casilla Prohibida       255
 	*/
 //Representación de la posición de las piezas de cada jugador
-	int tablero[8][8];
+	int tablero[10][12];
 
 public:
 	Tablero(void);
