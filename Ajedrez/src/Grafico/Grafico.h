87bb2866f8992efd156e08011c3c63b5379996e6
Enrique Heredia Aguado <enrique.he.ag@gmail.com>
2015-05-12 11:28:48 +0200

    Back to Glut.h

---
+++ Ajedrez/src/Grafico/Grafico.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "glut.h"
+
+class Grafico
+{
+public:
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+
+	Grafico(void);
+	~Grafico(void);
+
+	void inicializaTablero (void);
+	void dibujarPiezas (void);
+};
